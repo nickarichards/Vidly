78f5bac7540d9413e95d659b85dffd83f4d6ff5b
nick <richards.nicka@gmail.com>
2018-06-17 21:09:43 +0100

    Secion 2, Lecture 19 | Excercise

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,24 +10,21 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
-        public ActionResult Random()
+        
+        public ViewResult Index()
         {
-            var movie = new Movie() { Name = "Shrek!" };
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
-            };
+            var movies = GetMovies();
 
-            var viewModel = new RandomMoviewViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(viewModel);
+            return View(movies);
         }
 
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                //new Movie {Id = 1, Name = "Shrek"},
+                //new Movie {Id = 2, Name = "Wall-E"}
+            };
+        }
     }
 }
