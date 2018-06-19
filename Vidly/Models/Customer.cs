7a406294c1fc3e5fe3a01f5a22588fd375408b1d
nick <richards.nicka@gmail.com>
2018-06-19 17:58:42 +0100

    Section 4, Lecture 39 | Labels

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -12,9 +12,12 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-        public DateTime? BirthDate { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipTye { get; set; }
         public byte MembershipTypeId { get; set; }  
+
+        [Display(Name = "Date of Birth")]
+        public DateTime? BirthDate { get; set; }
+
     }
 }
