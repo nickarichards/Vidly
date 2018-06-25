e63a391c6e0cec575069d9f2ed4ca79e888139d6
nick <richards.nicka@gmail.com>
2018-06-25 19:43:08 +0100

    Section 8, Lecture 94 | Adding Profile Data

--- Vidly/Models/AccountViewModels.cs
+++ Vidly/Models/AccountViewModels.cs
@@ -64,6 +64,11 @@ namespace Vidly.Models
 
     public class RegisterViewModel
     {
+
+        [Required]
+        [Display(Name = "Driving License")]
+        public string DrivingLicense { get; set; }  
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
