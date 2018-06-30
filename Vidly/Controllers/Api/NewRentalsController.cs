d7367940e77012763fc519a00112d5091f2ea80f
nick <richards.nicka@gmail.com>
2018-06-30 13:39:04 +0100

    Section 10, Lecture 116 | Building the Simplest API

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -4,16 +4,48 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.UI;
+using AutoMapper;
 using Vidly.Dtos;
+using Vidly.Models;
 
 namespace Vidly.Controllers.Api
 {
     public class NewRentalsController : ApiController
     {
+
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+                _context = new ApplicationDbContext();
+        }
+
+        // POST /api/rental
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
-        {
-            throw new NotImplementedException();
+        {           
+            var customer = _context.Customers.Single(
+                c => c.Id == newRental.CustomerId);
+            
+            var movies = _context.Movies.Where(
+                m => newRental.MovieIds.Contains(m.Id));
+            
+            foreach (var movie in movies)
+            {
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Today
+                };
+                
+                _context.Rentals.Add(rental);
+            }
+            
+            _context.SaveChanges();
+
+            return Ok();
         }
 
 
