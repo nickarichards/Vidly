947de0706323eb26b10c195d59f78446b188ca57
nick <richards.nicka@gmail.com>
2018-06-30 11:45:31 +0100

    Section 10, Lecture 114 | Understanding the Problem

---
+++ Vidly/Dtos/NewRentalDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Profile;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class NewRentalDto
+    {
+        public int CustomerId { get; set; }
+        public List<int> MovieIds { get; set; }
+
+    }
+}
