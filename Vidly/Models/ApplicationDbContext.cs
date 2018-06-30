10bf1baa2fb27b516aad59bfa3f42154fb742f01
nick <richards.nicka@gmail.com>
2018-06-30 12:03:10 +0100

    Section 10, Lecture 115 | Domain Modelling

--- Vidly/Models/ApplicationDbContext.cs
+++ Vidly/Models/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.Entity;
+using System.Web.Profile;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Vidly.Models
@@ -9,6 +10,7 @@ namespace Vidly.Models
         public DbSet<Movie> Movies { get; set; }
         public DbSet<MembershipType> MembershipTypes { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
